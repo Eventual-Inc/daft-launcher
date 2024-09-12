cdb33d0c3443319c8655eb48481a329cc7ddd193
Raunak Bhagat <rabhagat31@gmail.com>
2024-09-12 16:49:35 -0700

    Attach down command to cli

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 import click
 from pathlib import Path
-from . import up
+from . import up, down
 
 
 @click.group()
@@ -10,6 +10,7 @@ def cli():
 
 def main():
     cli.add_command(up)
+    cli.add_command(down)
     cli()
 
 

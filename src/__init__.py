337537af2c96f22daac528317795b80013b7ce77
Raunak Bhagat <rabhagat31@gmail.com>
2024-09-12 16:44:15 -0700

    Implement spinning down of cluster

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,23 @@ from pathlib import Path
 import click
 import src.ray_default_configs
 from src.ray_default_configs import merge_config_with_default
-from ray.autoscaler.sdk import create_or_update_cluster
+from ray.autoscaler.sdk import create_or_update_cluster, teardown_cluster
+
+
+def get_final_config(
+    provider: Optional[str], config: Optional[Path],
+) -> dict | str:
+    if provider and config:
+        raise click.UsageError("Please provide either a provider or a config file.")
+    elif provider:
+        if provider == "aws":
+            return src.ray_default_configs.DEFAULT_AWS
+        else:
+            raise click.UsageError(f"Cloud provider {provider} not found")
+    elif config:
+        return merge_config_with_default(config)
+    else:
+        raise click.UsageError("Please provide either a provider or a config file.")
 
 
 @click.command("up", help="Spin the cluster up.")
@@ -22,18 +38,27 @@ from ray.autoscaler.sdk import create_or_update_cluster
     help="TOML configuration file.",
 )
 def up(provider: Optional[str], config: Optional[Path]):
-    if provider and config:
-        raise click.UsageError("Please provide either a provider or a config file.")
-    elif provider:
-        if provider == "aws":
-            final_config = src.ray_default_configs.DEFAULT_AWS
-        else:
-            raise click.UsageError(f"Cloud provider {provider} not found")
-    elif config:
-        final_config = merge_config_with_default(config)
-    else:
-        raise click.UsageError("Please provide either a provider or a config file.")
-
+    final_config = get_final_config(provider, config)
     create_or_update_cluster(
         final_config, no_restart=False, restart_only=False, no_config_cache=True
     )
+
+
+@click.command("down", help="Spin the cluster down.")
+@click.option(
+    "-p",
+    "--provider",
+    required=False,
+    type=click.STRING,
+    help="The cloud provider to use.",
+)
+@click.option(
+    "-c",
+    "--config",
+    required=False,
+    type=click.Path(exists=True),
+    help="TOML configuration file.",
+)
+def down(provider: Optional[str], config: Optional[Path]):
+    final_config = get_final_config(provider, config)
+    teardown_cluster(final_config)

43fb3064d0bc7d2eed7fe2d5de37a44c8385151e
Raunak Bhagat <rabhagat31@gmail.com>
2024-10-09 14:46:19 -0700

    Fix bug in which new object wasn't mutating the original ray_config

--- daft_launcher/configs.py
+++ daft_launcher/configs.py
@@ -133,7 +133,9 @@ def merge(
         for b_i in b:
             y = ray_config
             for b_ii in b_i[:-1]:
-                y = y.get(b_ii, {})
+                if b_ii not in y:
+                    y[b_ii] = {}
+                y = y[b_ii]
             y[b_i[-1]] = value
 
     setup_commands: list[str] = ray_config["setup_commands"]

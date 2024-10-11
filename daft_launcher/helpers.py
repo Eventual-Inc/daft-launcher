58d619d234a69adb84322458e5268c3f5a9241be
Raunak Bhagat <rabhagat31@gmail.com>
2024-10-11 10:16:09 -0700

    Finish todos

--- daft_launcher/helpers.py
+++ daft_launcher/helpers.py
@@ -90,60 +90,35 @@ def _parse_describe_instances_query() -> List[InstanceInformation]:
     ]
 
 
-# def _get_instance_id(config_bundle: data_definitions.ConfigurationBundle) -> str:
-#     custom_config, _ = config_bundle
-#     instance_infos = _parse_describe_instances_query()
-#     for instance_info in instance_infos:
-#         a = instance_info.state == "running"
-#         b = instance_info.get_tag("ray-node-type") == "head"
-#         c = instance_info.get_tag('ray-cluster-name') == custom_config.setup.name
-#         if a and b and c:
-#             return instance_info.instance_id
-
-
 def _get_ip(config_bundle: data_definitions.ConfigurationBundle) -> str:
     custom_config, _ = config_bundle
     instance_infos = _parse_describe_instances_query()
     for instance_info in instance_infos:
         a = instance_info.state == "running"
         b = instance_info.get_tag("ray-node-type") == "head"
-        c = instance_info.get_tag('ray-cluster-name') == custom_config.setup.name
+        c = instance_info.get_tag("ray-cluster-name") == custom_config.setup.name
         if a and b and c:
             return instance_info.ip
-    raise click.UsageError(f"Could not find the public ip of {custom_config.setup.name}'s head node.")
-    # custom_config, ray_config = config_bundle
-    # name = custom_config.setup.name
-    # region = custom_config.force_to_aws().region
-    # instance_groups: List[List[Any]] = _run_aws_command(
-    #     [
-    #         "aws",
-    #         "ec2",
-    #         "describe-instances",
-    #         "--region",
-    #         region,
-    #         "--filters",
-    #         "Name=tag:ray-node-type,Values=*",
-    #         "--query",
-    #         "Reservations[*].Instances[*].{State:State.Name,Tags:Tags,Ip:PublicIpAddress}",
-    #     ],
-    # )
-    # state_to_ips_mapping = _find_ip(instance_groups, name)
-    # if "running" not in state_to_ips_mapping:
-    #     raise click.UsageError(
-    #         f"The cluster {name} is not running; cannot connect to it."
-    #     )
-    # assert len(state_to_ips_mapping["running"]) <= 1
-    # if state_to_ips_mapping["running"]:
-    #     ip = state_to_ips_mapping["running"][0]
-    # else:
-    #     raise click.UsageError(
-    #         f"The cluster {name} is not running; cannot connect to it."
-    #     )
-    # if not ip:
-    #     raise click.UsageError(
-    #         f"The cluster {name} does not have a public IP address available."
-    #     )
-    # return ip
+    raise click.UsageError(
+        f"Could not find the public ip of {custom_config.setup.name}'s head node."
+    )
+
+
+def _get_public_keypair(
+    config_bundle: data_definitions.ConfigurationBundle,
+) -> str:
+    custom_config, _ = config_bundle
+    name = custom_config.setup.name
+    instance_infos = _parse_describe_instances_query()
+    for instance_info in instance_infos:
+        a = instance_info.state == "running"
+        b = instance_info.get_tag("ray-node-type") == "head"
+        c = instance_info.get_tag("ray-cluster-name") == custom_config.setup.name
+        if a and b and c:
+            return instance_info.keyname
+    raise click.UsageError(
+        f"Could not find the public keypair of {custom_config.setup.name}'s head node."
+    )
 
 
 def _run_aws_command(args: list[str]) -> Any:
@@ -161,39 +136,6 @@ def _run_aws_command(args: list[str]) -> Any:
         return json.loads(result.stdout)
     except json.JSONDecodeError as e:
         raise click.UsageError(f"Failed to parse AWS command output: {result.stdout}")
-
-
-def _find_ip(
-    instance_groups: List[List[Any]], name: str
-) -> dict[str, list[Optional[str]]]:
-    ip = None
-    state_to_ips_mapping: dict[str, list[Optional[str]]] = {}
-
-    def insert(state: str, ip: Optional[str]):
-        if state in state_to_ips_mapping:
-            state_to_ips_mapping[state].append(ip)
-        else:
-            state_to_ips_mapping[state] = [ip]
-
-    for instance_group in instance_groups:
-        for instance in instance_group:
-            is_head = False
-            cluster_name = None
-            state = instance["State"]
-            for tag in instance["Tags"]:
-                if tag["Key"] == "ray-cluster-name":
-                    cluster_name = tag["Value"]
-                elif tag["Key"] == "ray-node-type":
-                    is_head = tag["Value"] == "head"
-            if is_head and cluster_name == name:
-                insert(state, instance["Ip"])
-
-    if not state_to_ips_mapping:
-        raise click.UsageError(
-            f"The IP of the cluster with the name '{name}' not found."
-        )
-
-    return state_to_ips_mapping
 
 
 def _ssh_command(
@@ -220,6 +162,17 @@ def _ssh_command(
         + identity_args
         + [f"{user}@{ip}"]
     )
+
+
+def detect_keypair(config_bundle: data_definitions.ConfigurationBundle) -> Path:
+    public_keypair_name = _get_public_keypair(config_bundle)
+    path = Path("~").expanduser() / ".ssh" / f"{public_keypair_name}.pem"
+    if path.exists():
+        return path
+    else:
+        raise click.ClickException(
+            f"The public keypair name of the head node is called {public_keypair_name}, but no private keypair of that same name was found in the ~/.ssh directory; please re-run this command and manually pass in the path to the private keypair using the `-i <PATH_TO_KEY_PAIR>` flag."
+        )
 
 
 def ssh(
@@ -263,53 +216,6 @@ def get_state_map() -> dict[str, List[InstanceInformation]]:
             state_map[instance_info.state] = []
         state_map[instance_info.state].append(instance_info)
     return state_map
-
-
-def query_for_public_keypair(
-    config_bundle: data_definitions.ConfigurationBundle,
-) -> Optional[str]:
-    # custom_config, _ = config_bundle
-    # region = custom_config.force_to_aws().region
-    # instance_id = _get_instance_id(config_bundle)
-    # keys: List[List[Any]] = _run_aws_command(
-    #     [
-    #         "aws",
-    #         "ec2",
-    #         "describe-instances",
-    #         "--region",
-    #         region,
-    #         "--instance-ids",
-    #         instance_id,
-    #         "--query",
-    #         "Reservations[*].Instances[*].KeyName",
-    #     ],
-    # )
-    # assert len(keys) == 1
-    # instance_keys = keys[0]
-    # num_of_keys = len(instance_keys)
-    # if num_of_keys == 0:
-    #     return None
-    # elif num_of_keys == 1:
-    #     return instance_keys[0]
-    # else:
-    #     raise click.ClickException("This instance has multiple public key-pairs.")
-    ...
-
-
-def detect_keypair(config_bundle: data_definitions.ConfigurationBundle) -> Path:
-    ...
-    # if public_keypair_name := query_for_public_keypair(final_config):
-    #     path = Path("~").expanduser() / ".ssh" / f"{public_keypair_name}.pem"
-    #     if path.exists():
-    #         return path
-    #     else:
-    #         raise click.ClickException(
-    #             f"The public keypair name of the head node is called {public_keypair_name}, but no private keypair of that same name was found in the ~/.ssh directory; please re-run this command and manually pass in the path to the private keypair using the `-i <PATH_TO_KEY_PAIR>` flag."
-    #         )
-    # else:
-    #     raise click.UsageError(
-    #         "Could not detect keypair; please manually specify one by using the `-i <PATH_TO_KEY_PAIR>` flag."
-    #     )
 
 
 async def print_logs(logs):
